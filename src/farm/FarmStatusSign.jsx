c3946fc559726053554957c96d8b13e01070f526
白昱 <405237967@qq.com>
2025-12-30 23:06:40 +0800

    Initial commit

---
+++ src/farm/FarmStatusSign.jsx
@@ -0,0 +1,15 @@
+export default function FarmStatusSign() {
+    return (
+      <div
+        style={{
+          margin: '16px',
+          padding: '12px',
+          background: '#fff',
+          borderRadius: 8,
+        }}
+      >
+        🌞 今日樱桃：还没摘
+      </div>
+    )
+  }
+  
